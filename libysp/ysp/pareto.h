95d229b3f950bf5764afc5d871a440a61034fdc2
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2015-05-19 16:02:24 +0100

    pareto: ignore equal rewards in pareto front

--- libysp/ysp/pareto.h
+++ libysp/ysp/pareto.h
@@ -16,11 +16,17 @@ static inline int dominate(uint32_t n, rwd_t *r, rwd_t *p)
 {
 	uint32_t greater = 0;
 	uint32_t lesser = 0;
+	uint32_t equals = 0;
+	/* correct definition of pareto dominance? */
 	for (uint32_t i = 0; i < n; i++) {
 		greater += (r[i] > p[i]);
 		lesser += (r[i] < p[i]);
+		equals += (r[i] == p[i]);
 	}
-	if (greater == n)
+
+	if (equals == n)
+		return -2;
+	else if (greater == n)
 		return 1;
 	else if (lesser == n)
 		return -1;
